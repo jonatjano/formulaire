8df38efacc0dab08a772f0627b6267f9439c1cb3
jonatjano <jonathan.selle@laposte.net>
2018-01-08 10:18:24 +0100

    creation des classes basiques

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+import iut.algo.form.FormController;
+
+/**
+ * classe qui permet d'afficher un formulaire de test
+ * @author Team Infotik
+ * @version 2018-01-08
+ */
+public class Test
+{
+	public static void main(String[] args)
+	{
+		// FormController.afficherFormulaire("monFichier.xml");
+	}
+}
---
+++ src/iut/algo/form/FormController.java
@@ -0,0 +1,11 @@
+package iut.algo.form;
+
+/**
+ * classe permettant de controller les formulaires depuis des programmes exterieurs
+ * @author Team Infotik
+ * @version 2018-01-08
+ */
+public class FormController
+{
+
+}
