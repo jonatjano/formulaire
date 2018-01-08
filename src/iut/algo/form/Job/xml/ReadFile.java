a4dca4ee748f42fac451f5df69f4de4a432759b4
jonatjano <jonathan.selle@laposte.net>
2018-01-08 13:49:11 +0100

    renomage de LireFichier.java en ReadFile.java parceque la ça compile même pas ><

--- src/iut/algo/form/Job/xml/ReadFile.java
+++ src/iut/algo/form/Job/xml/ReadFile.java
@@ -14,40 +14,39 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
 public class ReadFile
-{
-	
+{	
 	public static void read(String file)
 	{
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
 		try
 		{
-			//Méthode qui permet d'activer la vérification du fichier
+			//Mï¿½thode qui permet d'activer la vï¿½rification du fichier
 			factory.setValidating(true);
 
 			DocumentBuilder builder = factory.newDocumentBuilder();
 
-			//création de notre objet d'erreurs
+			//crï¿½ation de notre objet d'erreurs
 			ErrorHandler errHandler = new SimpleErrorHandler();
-			//Affectation de notre objet au document pour interception des erreurs éventuelles
+			//Affectation de notre objet au document pour interception des erreurs ï¿½ventuelles
 			builder.setErrorHandler(errHandler);
 			File fileXML = new File(file);
 
 			//On rajoute un bloc de capture
-			//pour intercepter les erreurs au cas où il y en a
+			//pour intercepter les erreurs au cas oï¿½ il y en a
 
 			try
 			{
 				Document xml = builder.parse(fileXML);
 				Element root = xml.getDocumentElement();
-				
+
 				Element window = (Element) root.getFirstChild();
-				
+
 				Frame.CreateFrame(window);
 			}
 			catch (SAXParseException e){}
 
-		} 
+		}
 		catch (ParserConfigurationException e)
 		{
 			e.printStackTrace();
@@ -56,9 +55,9 @@ public class ReadFile
 		{
 			e.printStackTrace();
 		}
-		catch (IOException e) 
+		catch (IOException e)
 		{
 			e.printStackTrace();
-		} 
+		}
 	}
 }
