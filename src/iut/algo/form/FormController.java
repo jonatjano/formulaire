999725f3328a621aed4a181ea12d5014e0c353b1
jonatjano <jonathan.selle@laposte.net>
2018-01-08 13:46:44 +0100

    ajout de commentaires

--- src/iut/algo/form/FormController.java
+++ src/iut/algo/form/FormController.java
@@ -22,25 +22,36 @@ public class FormController
 	/**
 	 * methode appellée par une classe externe au package permettant d'appeller tous les utilitaires nécessaire au formulaire
 	 * @param filePath  Le chemin du fichier XML permettant de générer le formlaire
+	 *                  	Lu comme absolu sur classType est null
+	 *                  	Lu comme relatif si classType n'est pas null
 	 * @param classType La classe demandant le formulaire utilisée quand le filePath est un chemin relatif à la classe appellante
 	 */
 	public static void showForm(String filePath, Class classType)
 	{
+		// on cherche le chemin du fichier
 		String finalPath;
+		// c'est un chemin relatif à la classe
 		if (classType != null)
 		{
+			// on recupere le chemin menant à la classe auquel on ajoute le chemn relatif paramètre
 			finalPath = classType.getProtectionDomain().getCodeSource().getLocation().getPath() + "/" + filePath;
 		}
+		// c'est un chemin absolu
 		else
 		{
+			// on me juste le chemin
 			finalPath = filePath;
 		}
+		// creation du fichier
 		final File xmlFile = new File(finalPath);
-		System.out.println(xmlFile.getAbsolutePath());
+
+		// on fait les verifications basique d'existence du fichier
+		// s'il exist
 		if (!xmlFile.exists())
 		{
 			System.out.println("Le fichier entré n'existe pas");
 		}
+		// s'il possède bien une extension XML
 		else if (!xmlFile.getName().toUpperCase().endsWith(".XML"))
 		{
 			System.out.println("Le fichier entré ne correspond pas à un fichier XML");
